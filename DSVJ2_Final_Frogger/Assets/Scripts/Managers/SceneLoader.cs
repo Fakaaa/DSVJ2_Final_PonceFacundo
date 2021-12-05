ec819d3f333a12a38bb0ef07bba3d29fb58ecfa5
FacundoPonce <mrfacu125@gmail.com>
2021-12-05 11:35:56 -0300

    Coded sceneloader with loadingscreen.

--- DSVJ2_Final_Frogger/Assets/Scripts/Managers/SceneLoader.cs
+++ DSVJ2_Final_Frogger/Assets/Scripts/Managers/SceneLoader.cs
@@ -1,17 +1,75 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviourSingleton<SceneLoader>
 {
+    [SerializeField] private GameObject loaderCanvas;
+    [SerializeField] private Image progressBar;
+    [SerializeField] private float delayLoadFake;
+    [SerializeField] private int waitForActiveScene;
+    private float target;
+    private float maximumLoadOperation = 0.9f;
 
     void Start()
     {
-        
+        loaderCanvas.SetActive(false);
+        target = 0;
+        progressBar.fillAmount = 0;
     }
 
     void Update()
     {
-        
+        progressBar.fillAmount = Mathf.MoveTowards(progressBar.fillAmount, target, delayLoadFake * Time.deltaTime);
+    }
+
+    public async void LoadSceneAsyncWithProgressBar(string sceneName)
+    {
+        target = 0;
+        progressBar.fillAmount = 0;
+
+        var loadOperation = SceneManager.LoadSceneAsync(sceneName);
+        loadOperation.allowSceneActivation = false;
+        loaderCanvas.SetActive(true);
+
+        do
+        {
+            await Task.Delay(100);
+            target = loadOperation.progress;
+        } while (loadOperation.progress < maximumLoadOperation);
+
+        await Task.Delay(waitForActiveScene);
+
+        loadOperation.allowSceneActivation = true;
+        loaderCanvas.SetActive(false);
+    }
+
+    public void ReloadGameplay(int inSeconds)
+    {
+        IEnumerator WaitForLoadScene()
+        {
+            float t = 0;
+
+            while (t < inSeconds)
+            {
+                t += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+
+            SceneManager.LoadScene("Gameplay");
+            yield break;
+        }
+
+        StartCoroutine(WaitForLoadScene());
+    }
+
+    public void QuitApplication()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+        Application.Quit();
     }
 }

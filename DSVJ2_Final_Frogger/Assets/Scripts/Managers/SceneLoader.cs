b2015cd092991704b2383583805fc6cfbb2ef9f0
FacundoPonce <mrfacu125@gmail.com>
2021-12-05 11:18:35 -0300

    Created basic managers and monobeaviour singleton.

---
+++ DSVJ2_Final_Frogger/Assets/Scripts/Managers/SceneLoader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviourSingleton<SceneLoader>
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ DSVJ2_Final_Frogger/Assets/Scripts/Utilities/MonoBehaviourSingleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour where T : Component
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this as T;
+        DontDestroyOnLoad(this);
+    }
+}

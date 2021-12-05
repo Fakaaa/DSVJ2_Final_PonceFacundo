6eadf7c133f941b4cb40fa9a47de3295b650e9a5
FacundoPonce <mrfacu125@gmail.com>
2021-12-05 20:10:34 -0300

    Done first set of obstacles, tested flow of game with death of frog.

--- DSVJ2_Final_Frogger/Assets/Scripts/Player/Frog.cs
+++ DSVJ2_Final_Frogger/Assets/Scripts/Player/Frog.cs
@@ -1,7 +1,50 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
 
 public class Frog : MonoBehaviour
 {
-    [SerializeField] public int lives;
+    private Vector3 initialFrogPosition;
+    private int remainingLives;
 
+    public UnityAction onFrogDeath;
+    public UnityAction onFrogLose;
+    [SerializeField] LayerMask avoidCollision;
+    [SerializeField] Animator myAnimator;
+    BoxCollider colliderFrog;
+
+    void Start()
+    {
+        initialFrogPosition = transform.position;
+        colliderFrog = gameObject.GetComponent<BoxCollider>();
+    }
+
+    public void SetRemainingLives(int lives)
+    {
+        remainingLives = lives;
+    }
+
+    public void ResetFrogPosition()
+    {
+        if(remainingLives > 0)
+        {
+            transform.position = initialFrogPosition;
+            colliderFrog.enabled = true;
+            onFrogDeath?.Invoke();
+        }
+        else
+        {
+            onFrogLose?.Invoke();
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (!MyUtilities.Contains(avoidCollision, collision.gameObject.layer))
+        {
+            myAnimator.SetTrigger("Die");
+            colliderFrog.enabled = false;
+        }
+    }
 }

6c23ca0bbe9c81506c3dbb5c03a2ac59f4999241
FacundoPonce <mrfacu125@gmail.com>
2021-12-05 11:13:02 -0300

    Created simple main menu with basic buttons.

---
+++ DSVJ2_Final_Frogger/Assets/Scripts/UI_Elements/UI_MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI versionText;   
+
+    void Start()
+    {
+        versionText.text = "v" + Application.version;
+    }
+
+    void Update()
+    {
+        
+    }
+}
